665f5b01c38dcda829bf333f7eafcc40e38e1b87
Aldair Zamora <aldair.zamora@outlook.com>
2023-03-15 00:07:13 -0600

    initial commit

---
+++ javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/MyClass.java
@@ -0,0 +1,8 @@
+package xyz.azgcloudev.learnjava;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
---
+++ javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/Variables.java
@@ -0,0 +1,12 @@
+package xyz.azgcloudev.learnjava;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // string variable
+        String name = "Aldair";
+
+        System.out.println("My name is " + name + "!!"); // string variable and concatenation
+    }
+}
