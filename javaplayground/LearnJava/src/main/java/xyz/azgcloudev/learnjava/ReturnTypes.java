9a56f3b8b134db311d83d265830750eb1412fd16
Aldair Zamora <aldair.zamora@outlook.com>
2023-03-20 10:25:23 -0600

    working with return types

--- javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/ReturnTypes.java
+++ javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/ReturnTypes.java
@@ -3,6 +3,26 @@ package xyz.azgcloudev.learnjava;
 public class ReturnTypes {
 
     public static void main(String[] args) {
-        // TODO
+        System.out.println(addNumber(5,3));
+
+        System.out.println(fullName("Aldair", "Zamora"));
+
+        System.out.println(nameInitial("Aldair"));
+    }
+
+    // return int
+    public static int addNumber(int a , int b) {
+        return a + b;
+    }
+
+
+    // return string
+    public static String fullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+
+    // return a char
+    public static char nameInitial(String name) {
+        return name.charAt(0);
     }
 }
