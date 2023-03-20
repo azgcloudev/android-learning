4cc586f9a858cef80db8f39e2fe0a24aa56d0c99
Aldair Zamora <azgcloudev@Aldairs-MacBook-Air.local>
2023-03-20 10:58:00 -0600

    study return types

--- javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/ReturnTypes.java
+++ javaplayground/LearnJava/src/main/java/xyz/azgcloudev/learnjava/ReturnTypes.java
@@ -8,6 +8,15 @@ public class ReturnTypes {
         System.out.println(fullName("Aldair", "Zamora"));
 
         System.out.println(nameInitial("Aldair"));
+
+        // division
+        double division = divideNumbers(234, 43);
+        division++;
+        System.out.println(division);
+
+
+        String userBio = showBio("Aldair", "I love airplanes and motorcycles", 27);
+        System.out.println(userBio);
     }
 
     // return int
@@ -21,8 +30,20 @@ public class ReturnTypes {
         return firstName + " " + lastName;
     }
 
+    public static String showBio(String name, String bio, int age) {
+
+        return "My name is "
+                + name + " and I am " + age + " old. "
+                + bio + ".";
+    }
+
     // return a char
     public static char nameInitial(String name) {
         return name.charAt(0);
+    }
+
+    // return a double
+    public static double divideNumbers(double first, double second) {
+        return first / second;
     }
 }
